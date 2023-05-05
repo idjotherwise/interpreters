086f33cc2eef83608b67aaed1676a27016355abd
idjotherwise <ifan.johnston@gmail.com>
2023-05-05 23:49:01 +0200

    Full support for integer, boolean and return expressions in the evaluator

--- repl/repl.go
+++ repl/repl.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"monkey/evaluator"
 	"monkey/lexer"
 	"monkey/parser"
 )
@@ -18,34 +19,22 @@ func Start(in io.Reader, out io.Writer) {
 		if !scanned {
 			return
 		}
-		line := scanner.Text()
-		l := lexer.New(line)
+		r := scanner.Text()
+		l := lexer.New(r)
 		p := parser.New(l)
 		program := p.ParseProgram()
 		if len(p.Errors()) != 0 {
 			printParserErrors(out, p.Errors())
 		}
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, string(evaluated.Inspect()))
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
-const MONKEY_FACE = `
-					__,__
-		   .--. .-"     "-. .--.
-		  / .. \/  .-. .-.  \/ .. \
-		 | |  '|  /   Y   \  |'  | |
-		 | \   \  \ 0 | 0 /  /   / |
-		  \ '- ,\.-"""""""-./, -' /
-		   ''-' /_   ^ ^   _\ '-''
-		       |  \._   _./  |
-		       \   \ '~' /   /
-				'-._'.-.-'_.-'
-				'--~' '~--'
-`
-
 func printParserErrors(out io.Writer, errors []string) {
-	io.WriteString(out, MONKEY_FACE)
 	io.WriteString(out, "Woops! We ran into some monkey business here!\n")
 	io.WriteString(out, " parser errors:\n")
 	for _, msg := range errors {
