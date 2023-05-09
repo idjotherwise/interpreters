902bdd5b00eadc437eb763b622f77b33092ad17a
idjotherwise <ifan.johnston@gmail.com>
2023-05-10 00:07:01 +0200

    Start builtin functions

--- object/object.go
+++ object/object.go
@@ -20,7 +20,17 @@ const (
 
 	FUNCTION_OBJ = "FUNCTION"
 	STRING_OBJ   = "STRING"
+	BUILTIN_OBJ  = "BUILTIN"
 )
+
+type BuiltinFunction func(args ...Object) Object
+
+type Builtin struct {
+	Fn BuiltinFunction
+}
+
+func (b *Builtin) Type() ObjectType { return BUILTIN_OBJ }
+func (b *Builtin) Inspect() string  { return "builtin function" }
 
 type Object interface {
 	Type() ObjectType
