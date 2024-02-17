3f63b163d86880d193523e009d8b2389b3657aef
idjotherwise <ifan.johnston@gmail.com>
2024-02-17 13:02:41 +0000

    Start adding ast mod without macros

--- rust/src/ast/mod.rs
+++ rust/src/ast/mod.rs
@@ -1,1 +1,21 @@
+// pub enum Expr {
+//     Binary: struct {
+//         pub left: Box<Expr>,
+//         pub operator: Token,
+//         pub right: Box<Expr>,
+//     }
+// }
 
+use crate::token::Token;
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Expr {
+    Binary,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Binary {
+    pub left: Box<Expr>,
+    pub operator: Token,
+    pub right: Box<Expr>,
+}
